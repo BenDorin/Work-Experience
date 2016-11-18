7156af2ca69829e9edc73981ad156d04abd53d6a
Ben Dorin <ben.dorin@aridhia.com>
2016-11-18 14:34:43 +0000

    Moving files into folder so that they can be manipulated by the sinatra (ruby) file, also added new code to sinatra meaning I can change the title names of the html webpages from the ruby file, also changed the HTML to .ERB so that they can be maniluated an read by sinatra/ruby

--- rubyontheweb.rb
+++ rubyontheweb.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 #tells the ruby file we will be using sinatra in the code
-require 'rubygems' if RUBY_VERSION < "1.9"
-require 'sinatra/base'
+
+require 'sinatra'
 
 #runs sinatra web server
-class MyApp < Sinatra::Base
-  get '/' do
-    'Hello World!'
-  end
+
+get '/' do
+	@foo_title = "My Fable Generator"
+	erb :page1
 end
 
-run MyApp.new
+post '/' do
+
+end
+
+get '/page2' do
+	@foo_title = "My Fable Generator"
+	erb :page2
+end
+
