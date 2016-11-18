3346bf108a0330dc5a3f6c96757397f1e5640d09
Ben Dorin <ben.dorin@aridhia.com>
2016-11-18 12:14:20 +0000

    Adding All the new created files and modified previous file! Addign new files to git for the work experience project website!

---
+++ config.ru
@@ -0,0 +1,3 @@
+require_relative 'rubyontheweb'
+
+run MyApp.new
--- rubyontheweb.rb
+++ rubyontheweb.rb
@@ -1,1 +1,12 @@
-#new git rb file!
+# frozen_string_literal: true
+#tells the ruby file we will be using sinatra in the code
+require 'rubygems' if RUBY_VERSION < "1.9"
+require 'sinatra/base'
+
+#runs sinatra web server
+class MyApp < Sinatra::Base
+  get '/' do
+    'Hello World!'
+  end
+end
+
